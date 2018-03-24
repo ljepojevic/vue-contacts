d750633e0d3993e25027456a5b6780175d4adea6
Milan Ljepojevic <ljepojevicm@yahoo.com>
2018-03-24 22:46:59 +0100

    Create and Edit form

--- resources/assets/js/components/Contacts.vue
+++ resources/assets/js/components/Contacts.vue
@@ -1,14 +1,53 @@
 <template>
-	<h1>Contacts</h1>
+	<div>
+		<h1>Contacts</h1>
+		<form action="#" @submit.prevent="edit ? updateContact(contact.id) : createContact()">
+			<div class="form-group">
+				<label>Name</label>
+				<input v-model="contact.name" type="text" name="name" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email</label>
+				<input v-model="contact.email" type="text" name="email" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Phone</label>
+				<input v-model="contact.phone" type="text" name="phone" class="form-control">
+			</div>
+			<div class="form-group">
+				<button v-show="!edit" type="submit" class="btn btn-primary">New Contact</button>
+				<button v-show="edit" type="submit" class="btn btn-primary">Update Contact</button>
+			</div>
+		</form>	
+	</div>
 </template>
 
 <script>
 	export default {
 		data: function(){
-			return {}
+			return {
+				edit: false,
+				list: [],
+				contact: {
+					id: '',
+					name: '',
+					email: '',
+					phone: ''
+				}
+			}
 		},
 		mounted: function(){
 			console.log('Console component loaded');
+		},
+		methods: {
+			createContact: function(){
+				console.log('Creating contact...');
+				return;
+			},
+			updateContact: function(id){
+				console.log('Updating '+ id +' contact...');
+				return;
+			}
 		}
 	}
 </script>
