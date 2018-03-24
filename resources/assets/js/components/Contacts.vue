c62967084186dd44726d484f6e684f6e133d5218
Milan Ljepojevic <ljepojevicm@yahoo.com>
2018-03-24 22:26:14 +0100

    Added Vue Contacts component

---
+++ resources/assets/js/components/Contacts.vue
@@ -0,0 +1,14 @@
+<template>
+	<h1>Contacts</h1>
+</template>
+
+<script>
+	export default {
+		data: function(){
+			return {}
+		},
+		mounted: function(){
+			console.log('Console component loaded');
+		}
+	}
+</script>
